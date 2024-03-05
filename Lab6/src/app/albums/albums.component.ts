6a15f7a73e3595a088d868081e3909d01a58dc1a
Daniyal Tuzelbayev <danchik@Daniyals-MacBook-Pro.local>
2024-03-05 14:56:16 +0500

    lab6 uploaded

--- Lab6/src/app/albums/albums.component.ts
+++ Lab6/src/app/albums/albums.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import {RouterModule} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
+import {AlbumsService} from "../albums.service";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-albums',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.scss'
 })
 export class AlbumsComponent implements OnInit {
   albums: any[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private albumsService: AlbumsService) { }
 
   ngOnInit(): void {
-    this.fetchAlbums();
-  }
-
-  fetchAlbums() {
-    this.http.get<any[]>('https://jsonplaceholder.typicode.com/albums')
-      .subscribe(albums => this.albums = albums);
+    this.albumsService.getAlbums().subscribe(albums => {
+      this.albums = albums;
+    });
   }
 
   deleteAlbum(albumId: number) {
-    // Implement delete functionality here
-    console.log('Deleting album with ID:', albumId);
+    this.albumsService.deleteAlbum(albumId).subscribe(() => {
+      // Remove the deleted album from the list
+      this.albums = this.albums.filter(album => album.id !== albumId);
+    });
   }
 
 }
