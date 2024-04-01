1535c39b5293bd5780704dd8d63b3c039d3ea552
holydanchik <tuzelbaeff@gmail.com>
2024-04-01 14:42:40 +0500

    settings.py fixed

--- Lab8/shop_back/api/views.py
+++ Lab8/shop_back/api/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Product, Category
+
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    data = {'products': list(products.values())}
+    return JsonResponse(data)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    data = {'product': {
+        'name': product.name,
+        'price': product.price,
+        'description': product.description,
+        'count': product.count,
+        'is_active': product.is_active,
+        'category': product.category.name
+    }}
+    return JsonResponse(data)
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    data = {'categories': list(categories.values())}
+    return JsonResponse(data)
+
+
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    data = {'category': {
+        'name': category.name,
+    }}
+    return JsonResponse(data)
+
+
+def category_products(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    products = category.products.all()
+    data = {'products': list(products.values())}
+    return JsonResponse(data)
